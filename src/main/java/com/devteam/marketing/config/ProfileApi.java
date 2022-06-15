4284a8bbf05d757a8cfe1bbec7e41d2e68c9df0a
hijinhoon <hijinhoon@muhanit.kr>
2022-06-15 18:08:43 +0900

    feat: ProfileApi 로직 real 설정 추가

--- src/main/java/com/devteam/marketing/config/ProfileApi.java
+++ src/main/java/com/devteam/marketing/config/ProfileApi.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,9 +16,14 @@ public class ProfileApi {
 
     @GetMapping("/profile")
     public String profile() {
-        return Arrays.stream(env.getActiveProfiles())
-                .findFirst()
-                .orElse("");
+        final List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        final List<String> realProfiles = Arrays.asList("rea1", "real2");
+        final String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 
 }
