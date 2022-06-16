346f02220df60994dd4db032f69c5d4ef34c7c7e
songjinhoon <jinhoon0319@gmail.com>
2022-06-16 21:24:01 +0900

    fix: switch.sh 오타 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 function switch_proxy() {
-    IDLE_PORT=$(fine_idle_port)
+    IDLE_PORT=$(find_idle_port)
 
     echo "> 전환할 port: $IDLE_PORT"
     echo "> Port 전환"
